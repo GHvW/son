3fac04217656323bdec3d8ba59bd42b7d09292d9
Garrett van Wageningen <ghvanw283@gmail.com>
2021-03-01 14:09:02 -0600

    send it up

---
+++ src/bible/favorite_verses.rs
@@ -0,0 +1,10 @@
+
+func favorites() -> Vec<&str> {
+    vec![
+        ("ESV", "Matthew 22:35‭-‬40", r"And one of them, a lawyer, asked him a question to test him. 
+        “Teacher, which is the great commandment in the Law?” 
+        And he said to him, “You shall love the Lord your God with all your heart and with all your soul and with all your mind.  
+        This is the great and first commandment.  And a second is like it: You shall love your neighbor as yourself.  
+        On these two commandments depend all the Law and the Prophets.")
+    ]
+}
